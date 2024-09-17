aa064ca7dd64f5f4c180e263ac4da09cb4655b4f
lafbeauty <6732792+lafbeauty@users.noreply.github.com>
2024-09-16 21:51:57 -0400

    API fetch completed

--- js/index.js
+++ js/index.js
@@ -105,3 +105,21 @@ document.addEventListener("DOMContentLoaded", function () {
     messageForm.reset()
   })
 })
+
+fetch(`https://api.github.com/users/lafbeauty/repos`)
+  .then((response) => response.json())
+  .then((data) => {
+    const repositories = data
+    console.log(repositories)
+
+    const projectSection = document.getElementById("projects-section")
+    const projectList = projectSection.querySelector("ul")
+
+    for (let i = 0; i < repositories.length; i++) {
+      const project = document.createElement("li")
+      project.innerText = repositories[i].name
+      projectList.appendChild(project)
+    }
+  })
+
+  .catch((error) => console.log("not working", error))
